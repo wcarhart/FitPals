881d4fda0d7731e17bc4a023b3ead6625812613e
gdaryanani <gdaryanani@gautams-macbook-pro.local>
2018-05-02 22:17:58 -0700

    Created new UIView for Purchase Page

--- FitPals/Controller/Market/PurchaseTableViewController.swift
+++ FitPals/Controller/Market/PurchaseTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PurchaseTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ FitPals/Controller/Market/PurchaseViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PurchaseViewController.swift
+//  FitPals
+//
+//  Created by Gautam Daryanani on 5/2/18.
+//  Copyright © 2018 Will Carhart. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseViewController: UIViewController {
+    
+    var cart : [Product] = []
+    @IBOutlet weak var cartTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cart.count // your number of cell here
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // your cell coding
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // cell selected code here
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
